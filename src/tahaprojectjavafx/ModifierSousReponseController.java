8d0315f01f09a6033d5a15acac8b1d77dc83d49d
taha ch <arthas@DESKTOP-6U1NNM8>
2019-02-25 17:42:10 +0100

    ...

--- src/tahaprojectjavafx/ModifierSousReponseController.java
+++ src/tahaprojectjavafx/ModifierSousReponseController.java
@@ -5,6 +5,8 @@
  */
 package tahaprojectjavafx;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXDrawer;
 import java.io.IOException;
 import java.net.URL;
 import java.sql.SQLException;
@@ -19,7 +21,14 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextArea;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
 /**
@@ -35,6 +44,151 @@ public class ModifierSousReponseController implements Initializable {
     private Button btn1;
     @FXML
     private Button btn2;
+    @FXML
+    private AnchorPane home;
+    @FXML
+    private ScrollPane scrollHomePage;
+    @FXML
+    private AnchorPane mainAP;
+    @FXML
+    private JFXDrawer menu;
+    @FXML
+    private AnchorPane menuBar;
+    @FXML
+    private AnchorPane loginRegisterProfie;
+    @FXML
+    private HBox profileHB;
+    @FXML
+    private Circle photoProfilMenu;
+    @FXML
+    private HBox menuHB;
+    @FXML
+    private VBox profileVB;
+    @FXML
+    private JFXButton profilMenu;
+    @FXML
+    private JFXButton settingsProfile;
+    @FXML
+    private Label nameUserMenu;
+    @FXML
+    private Label labelLastLoginMenu;
+    @FXML
+    private Label LastLoginMenu;
+    @FXML
+    private JFXButton logoutMenu;
+    @FXML
+    private JFXButton admin;
+    @FXML
+    private AnchorPane barreRecherche;
+    @FXML
+    private VBox menuVB;
+    @FXML
+    private VBox accueilVB;
+    @FXML
+    private VBox catVB;
+    @FXML
+    private VBox sousMenuCat;
+    @FXML
+    private VBox eventVB;
+    @FXML
+    private JFXButton eventButton;
+    @FXML
+    private VBox sousMenuEvent;
+    @FXML
+    private VBox blogVB;
+    @FXML
+    private JFXButton blogButton;
+    @FXML
+    private VBox sousMenuBlog;
+    @FXML
+    private JFXButton ajoutArticleB;
+    @FXML
+    private JFXButton listArticleB;
+    @FXML
+    private VBox dealVB;
+    @FXML
+    private JFXButton dealButton;
+    @FXML
+    private VBox sousMenuDeal;
+    @FXML
+    private JFXButton catButton;
+     @FXML
+      private AnchorPane addArticle;
+    @FXML
+    private JFXButton eventButton1;
+    @FXML
+    private JFXButton eventButton11;
+
+
+    
+    @FXML
+    private void AddArticle(MouseEvent event) throws IOException {
+                Parent root = FXMLLoader.load(getClass().getResource("/vue/AddArticle.fxml"));
+     ajoutArticleB.getScene().setRoot(root);
+    }
+
+    @FXML
+    private void ShowListArticle(MouseEvent event) 
+        throws IOException {
+                Parent root = FXMLLoader.load(getClass().getResource("/vue/ListArticles.fxml"));
+     listArticleB.getScene().setRoot(root);
+    }
+    
+@FXML
+private void AcceuilButtonAction(ActionEvent event) throws IOException
+{
+    try {
+        Parent home_page_parent = FXMLLoader.load(getClass().getResource("Acceuil.fxml"));
+        Scene home_page_scene = new Scene(home_page_parent);
+        Stage app_stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        app_stage.setScene(home_page_scene);
+        app_stage.show();
+        
+    } catch (IOException ex) {
+        Logger.getLogger(MesQuestionsController.class.getName()).log(Level.SEVERE, null, ex);
+    }
+  
+}
+
+
+
+
+@FXML
+private void PublierQuestionButtonAction(ActionEvent event) throws IOException
+{
+    try {
+        Parent home_page_parent = FXMLLoader.load(getClass().getResource("PublierQuestion.fxml"));
+        Scene home_page_scene = new Scene(home_page_parent);
+        Stage app_stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        app_stage.setScene(home_page_scene);
+        app_stage.show();
+        
+    } catch (IOException ex) {
+        Logger.getLogger(MesQuestionsController.class.getName()).log(Level.SEVERE, null, ex);
+    }
+  
+}
+
+
+
+
+@FXML
+private void MesQuestionButtonAction(ActionEvent event) throws IOException
+{
+    try {
+        Parent home_page_parent = FXMLLoader.load(getClass().getResource("MesQuestions.fxml"));
+        Scene home_page_scene = new Scene(home_page_parent);
+        Stage app_stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        app_stage.setScene(home_page_scene);
+        app_stage.show();
+        
+    } catch (IOException ex) {
+        Logger.getLogger(MesQuestionsController.class.getName()).log(Level.SEVERE, null, ex);
+    }
+  
+}
+
+
     
     
     
